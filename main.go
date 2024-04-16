3d9f66461ea1fadb49ee5e540530e7185830759a
SonLN <sonln@ocb.com.vn>
2024-04-16 14:32:33 +0700

    add get post api test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go/rest/api/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
