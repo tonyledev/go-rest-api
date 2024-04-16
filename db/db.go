7e306d23b9dd72297994dc47a73884e1d7d52ab0
SonLN <sonln@ocb.com.vn>
2024-04-16 16:00:47 +0700

    Add save event to db

--- db/db.go
+++ db/db.go
@@ -17,4 +17,23 @@ func InitDB() {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+	_, err := DB.Exec(createEventTable)
+	if err != nil {
+		panic("Could not create event table")
+	}
 }
