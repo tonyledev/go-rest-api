341b5f7549c951a944bea683694b9229a3838d2d
tonyle <lengocsontdt@gmail.com>
2024-04-20 16:21:08 +0700

    add hash password using bcrypt

--- models/users.go
+++ models/users.go
@@ -1,6 +1,9 @@
 package models
 
-import "go/rest/api/db"
+import (
+	"go/rest/api/db"
+	"go/rest/api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -17,7 +20,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
