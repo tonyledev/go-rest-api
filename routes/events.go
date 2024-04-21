794242e0c4e6de4e14364f191ed092ac19648b52
tonyle <lengocsontdt@gmail.com>
2024-04-21 14:45:49 +0700

    enhance the middlewares

--- routes/events.go
+++ routes/events.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"fmt"
 	"go/rest/api/models"
-	"go/rest/api/utils"
 	"net/http"
 	"strconv"
 
@@ -34,26 +33,13 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
 	}
-
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
